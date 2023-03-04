42d2544745233ee24cabd3ef4222e3b1aa638d3e
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2023-03-04 08:43:33 -0300

    feat: configurando css e add alertas nos inputs

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -10,10 +10,17 @@ const [weight, setWeight]= useState(null)
 const [messageImc, setMessageImc]= useState("preencha o peso e altura")
 const [imc, setImc]= useState(null)
 const [textButton, setTextButton]= useState("Calcular")
+const [errorMessage, setErrorMessage] = useState(null)
 
 
 function imcCalculator(){
   return setImc((weight/(height*height)).toFixed(2))
+}
+
+function verificationImc(){
+  if(imc == null){
+    setErrorMessage("*campo obrigatório*")
+  }
 }
 
 function validationImc(){
@@ -23,17 +30,21 @@ function validationImc(){
     setWeight(null)
     setMessageImc("Seu imc é igual:")
     setTextButton("Calcular Novamente")
+    setErrorMessage(null)
     return
   }
+  verificationImc()
   setImc(null)
   setTextButton("Calcular")
   setMessageImc("preencha o peso e altura")
+  
 }
   return(
     <View style={styles.formContext}>
       <View style={styles.form}>
 
         <Text style={styles.formLabel}>Altura</Text>
+        <Text style={styles.errorMessage}>{errorMessage}</Text>
         <TextInput
         style={styles.input}
         onChangeText={setHeight}
@@ -43,6 +54,7 @@ function validationImc(){
         />
 
         <Text style={styles.formLabel}>Peso</Text>
+        <Text style={styles.errorMessage}>{errorMessage}</Text>
         <TextInput
         style={styles.input}
         onChangeText={setWeight}
