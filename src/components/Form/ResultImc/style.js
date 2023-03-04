0286d8756754bb728264fdae38e47466ae35ef67
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2023-03-04 09:26:20 -0300

    feat: add API Share e configurando css

--- src/components/Form/ResultImc/style.js
+++ src/components/Form/ResultImc/style.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-  resultImc: {
+  contextImc: {
    flex: 1,
    marginTop: 15,
    paddingTop:60,
@@ -18,8 +18,23 @@ const styles = StyleSheet.create({
     fontSize: 18,
     color:"#ff0043",
     fontWeight:"bold",
+  },
+  boxShareButton: {
+    width: "100%",
+    alignItems: "center",
+    marginBottom:10,
+  },
+  share: {
+    backgroundColor: "#1b77f2",
+    borderRadius: 50,
+    paddingTop: 5,
+    paddingBottom:5, 
+  },
+  sharedText: {
+    color: "#ffffff",
+    fontWeight: "bold",
+    paddingHorizontal: 30,
   }
- 
 });
 
 export default styles
