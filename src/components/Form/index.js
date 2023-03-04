9db5b900c06522eea30a9b61f17c2a6c61b4efc3
Afonso Henrique <89816770+aefonso@users.noreply.github.com>
2023-03-03 22:15:24 -0300

    feat: configurando css

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,6 +1,7 @@
 import React, {useState} from "react";
-import {View, TextInput, Text, Button} from 'react-native';
+import {View, TextInput, Text, TouchableOpacity} from 'react-native';
 import ResultImc from "./ResultImc";
+import styles from "./style";
 
 export default function Form(){
 
@@ -29,28 +30,34 @@ function validationImc(){
   setMessageImc("preencha o peso e altura")
 }
   return(
-    <View>
-      <View>
+    <View style={styles.formContext}>
+      <View style={styles.form}>
 
-        <Text>Altura</Text>
+        <Text style={styles.formLabel}>Altura</Text>
         <TextInput
+        style={styles.input}
         onChangeText={setHeight}
         value={height}
         placeholder="ex. 1.75"
         KeyboardType="numeric"
         />
 
-        <Text>Peso</Text>
+        <Text style={styles.formLabel}>Peso</Text>
         <TextInput
+        style={styles.input}
         onChangeText={setWeight}
         value={weight}
         placeholder="ex. 80.475"
         KeyboardType="numeric"
         />
-        <Button 
-          onPress={() => validationImc()}
-          title={textButton}
-        />
+      <TouchableOpacity
+      style={styles.ButtonCalculator}
+      onPress={() => {
+        validationImc()
+      }}
+      >
+        <Text style={styles.textCalculator}>{textButton}</Text>
+      </TouchableOpacity>
       </View>
       <ResultImc messageResultImc={messageImc} resultImc={imc}/>
     </View>
